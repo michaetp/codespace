55180166bd495d1593f9101171ad5af7cd3c5b86
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-02 22:57:24 +0000

    Mon, Oct 2, 2023, 10:57 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //ask for height input
+    int h;
+    h = get_int
+    //print first row of pyramid
+    //make algorithm for pyramid structure (growing base)
 }

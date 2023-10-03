d5429f5c389e59a2def668a0da3179a94398fe71
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-03 21:42:29 +0000

    Tue, Oct 3, 2023, 9:42 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,8 +9,17 @@ int main(void)
         credit = get_long("16-Digit Number: ");
     }
     while ((credit / 1000000000000000) < 1 || (credit / 1000000000000000) > 10);
+
 }
 
 int everyotherdigit(long credit)
-
-credit
+{
+    int sum = 0;
+    while (credit > 0)
+    {
+        int lastdigit = credit % 10;
+        credit = credit / 10;
+        sum = sum + (lastdigit * 2);
+    }
+    return sum;
+}

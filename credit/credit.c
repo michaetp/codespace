b41f48d1bc802110ccf61c4319a4fd7cb7e35330
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-03 23:54:55 +0000

    Tue, Oct 3, 2023, 11:54 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -17,6 +17,7 @@ int main(void)
     }
     printf("%i\n", countno);
 
+//Check if AMEX
     if(countno == 15)
     {
         long first_two_digits = originalcredit / pow(10,13);
@@ -26,5 +27,20 @@ int main(void)
         }
     }
 
+//Check if MasterCard or VISA
+    if(countno == 16)
+    {
+        long first_two_digits = originalcredit / pow(10,14);
+        if(first_two_digits == 51 || first_two_digits == 52 || first_two_digits == 53 || first_two_digits == 54 || first_two_digits == 55)
+        {
+            printf("MASTERCARD\n");
+        }
+
+        long first_digit = 
+        if(first_two_digits == 34 || first_two_digits == 37)
+        {
+            printf("VISA\n");
+        }
+    }
 }
 

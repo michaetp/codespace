d4df87fc97f30eb137fd9b8ee1387fee6419cc49
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-03 22:51:34 +0000

    Tue, Oct 3, 2023, 10:51 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,7 +8,10 @@ int main(void)
     long credit;
     credit = get_long("16-Digit Number: ");
 
-    for (int countno = 0, )
+    for (int credit = 0; (credit/10) == 0; credit++)
+    {
+        
+    }
 
 }
 

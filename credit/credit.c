beae35c5e209dbc37d5362eb86154f78a3a68697
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-03 23:37:08 +0000

    Tue, Oct 3, 2023, 11:37 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int isAMEX(int countno);
-
 int main(void)
 {
     long credit;
@@ -16,18 +14,12 @@ int main(void)
     }
 
     if(countno == 15)
-
-}
-
-int isAMEX(int countno)
-{
-    int countno;
-    if(countno == 15)
     {
         if((credit / (10^13)) == 34 || (credit / (10^13)) == 37)
         {
             printf("AMEX/n");
         }
     }
-    
+
 }
+

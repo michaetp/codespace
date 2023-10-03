726a21b3fe7a19b0373b364907e53582fb556a8f
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-03 23:26:28 +0000

    Tue, Oct 3, 2023, 11:26 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,19 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int everyotherdigit(long credit);
 
 int main(void)
 {
     long credit;
     credit = get_long("16-Digit Number: ");
 
-    int countno = 0;
-
-    for (; credit > 0; credit = credit / 10)
+    int countno = 1;
+    while ((credit/10) > 0)
     {
-        countno++;
+    credit = (credit / 10);
+    countno = countno + 1;
     }
-
     printf("%i\n", countno);
 }
 

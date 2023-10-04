de51c4acf32e231e820421a8d49050435b22894b
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-04 03:02:10 +0000

    Wed, Oct 4, 2023, 3:02 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,10 +22,39 @@ int main(void)
         countno = countno + 1;
     }
 
+long lastno;
+    long s_lastno;
+    int slp1;
+    int slp2;
+    long total;
+    while(Luhncredit >= 10)
+    {
+        lastno = Luhncredit % 10;
+        s_lastno = (Luhncredit / 10) % 10;
+        if((2 * s_lastno) > 9)
+        {
+            slp1 = s_lastno % 10;
+            slp2 = s_lastno / 10;
+            total = total + lastno + slp1 + slp2;
+        }
+        else
+        {
+            total = total + lastno + (2 * s_lastno);
+        }
+        Luhncredit = Luhncredit / 100;
+    }
+    while(Luhncredit < 10 && Luhncredit > 0)
+    {
+        lastno = Luhncredit % 10;
+        total = total + lastno;
+        Luhncredit = Luhncredit / 10;
+    }
+
 //Check if Luhn algorithm pass
     if(LUHN(Luhncredit) == false)
     {
         printf("INVALID\n");
+        printf("%li\n", total);
     }
     else
     {

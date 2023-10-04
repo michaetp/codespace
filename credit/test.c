2432b121e53022da04178117532a3f754c0afbea
michaetp <146770660+michaetp@users.noreply.github.com>
2023-10-04 03:03:34 +0000

    Wed, Oct 4, 2023, 3:03 AM +00:00

--- credit/test.c
+++ credit/test.c
@@ -4,8 +4,32 @@
 
 int main(void)
 {
-    long c = get_long("Number: ");
-    int m = (c/10) % 10;
-
-    printf("%i, %li\n", m,c);
+    long lastno;
+    long s_lastno;
+    int slp1;
+    int slp2;
+    long total;
+    while(Luhncredit >= 10)
+    {
+        lastno = Luhncredit % 10;
+        s_lastno = (Luhncredit / 10) % 10;
+        if((2 * s_lastno) > 9)
+        {
+            slp1 = s_lastno % 10;
+            slp2 = s_lastno / 10;
+            total = total + lastno + slp1 + slp2;
+        }
+        else
+        {
+            total = total + lastno + (2 * s_lastno);
+        }
+        Luhncredit = Luhncredit / 100;
+    }
+    while(Luhncredit < 10 && Luhncredit > 0)
+    {
+        lastno = Luhncredit % 10;
+        total = total + lastno;
+        Luhncredit = Luhncredit / 10;
+    }
+    printf("%li\n", total)
 }
